aef956078026ccb4ff29fa2c484f8dc0106806fc
pjehan <pierre.jehan@gmail.com>
2020-06-08 00:38:51 +0200

    Ajout d'un shortcut dans les fixtures

--- src/DataFixtures/ShortcutFixtures.php
+++ src/DataFixtures/ShortcutFixtures.php
@@ -18,11 +18,22 @@ class ShortcutFixtures extends Fixture implements DependentFixtureInterface
         $indentPhpStorm->addCategory($this->getReference('php'));
         $indentPhpStorm->addCategory($this->getReference('js'));
         $indentPhpStorm->setWindows('Ctrl Alt i');
-        $indentPhpStorm->setMacos('Ctrl Alt l');
+        $indentPhpStorm->setMacos('Command Alt l');
         $indentPhpStorm->setLinux('Ctrl Alt l');
         $indentPhpStorm->setContext('Dans un fichier .php');
         $indentPhpStorm->setDescription('Indentation automatique du code PHP et suppression des espaces inutiles.');
         $manager->persist($indentPhpStorm);
+
+        $duplicateLineVscode = new Shortcut();
+        $duplicateLineVscode->setTitle("Dupliquer une ligne dans VSCode");
+        $duplicateLineVscode->setSoftware($this->getReference('vscode'));
+        $duplicateLineVscode->addCategory($this->getReference('development'));
+        $duplicateLineVscode->setWindows('Ctrl Shift d');
+        $duplicateLineVscode->setMacos('Command Shift d');
+        $duplicateLineVscode->setLinux('Ctrl Shift d');
+        $duplicateLineVscode->setContext('Dans un fichier code');
+        $duplicateLineVscode->setDescription('Duplication de la ligne sur laquelle se trouve le curseur dans un fichier.');
+        $manager->persist($duplicateLineVscode);
 
         $manager->flush();
     }
